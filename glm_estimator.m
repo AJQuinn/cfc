06d876d82e2c70adbae746996e30e9507bf2fb5d
Andrew Quinn <andrew@HAL9000-2.local>
2015-02-06 13:40:18 +0000

    change glm_estimator to only return R2

--- glm_estimator.m
+++ glm_estimator.m
@@ -1,4 +1,4 @@
-function [glm] = glm_estimator(modulating_signal,modulated_signal, distr)
+function [r2] = glm_estimator(modulating_signal,modulated_signal, distr)
 
 % Function for estimating the GLM phase amplitude coupling as defined in Penny et al 2008
 % Modulating signal is the theta phase and the modulated signal is the gamma amplitude
@@ -22,5 +22,5 @@ glm.stats = stats;
 ss_data = sum( modulated_signal .* modulated_signal);
 ss_resid = sum ( stats.resid .* stats.resid );
 
-glm.r2 = (ss_data - ss_resid) / ss_data;
+r2 = (ss_data - ss_resid) / ss_data;
 
