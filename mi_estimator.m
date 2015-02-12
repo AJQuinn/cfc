470b330b941f506aa19af5c8c272f7b83dbd59f4
Andrew Quinn <andrew.quinn@psych.ox.ac.uk>
2015-02-12 00:44:10 +0000

    Update modulation index estimator

--- mi_estimator.m
+++ mi_estimator.m
@@ -1,15 +1,9 @@
-function [mean_vector] = canolty_estimator(modulating_signal,modulated_signal)
+function [mean_vector] = mi_estimator(modulating_signal,modulated_signal)
 %
 % Function estimating the mean vector as defined in Canolty et al 2006
 
-% Get the phase time series from the modulating signal
-phase = angle(hilbert(modulating_signal));
-
-% Get the amplitude time series from the modualted signal
-amp = abs(hilbert(modulated_signal));
-
 % Combine into complex form
-Z = amp .* exp(1i * phase);
+Z = modulated_signal .* exp(1i * modulating_signal);
 
 % Compute mean vector
 mean_vector = abs(mean(squeeze(Z)));
