91a3a8a9dc0ac46e4b7668a414248a3efe2d0d9f
Andrew Quinn <andrew.quinn@psych.ox.ac.uk>
2015-06-22 16:12:58 +0100

    Tweaks to plotting/plot_comodulogram

--- plotting/plot_comodulogram.m
+++ plotting/plot_comodulogram.m
@@ -1,7 +1,8 @@
-function plot_comodulogram(cmg,spectrum,freq_vect)
+function plot_comodulogram(cmg,spectrum,freq_vect,log_high,col_levels)
 
 if nargin < 2
 
+    %% Just plot the cmg without any fancy spectrum sidebars
     figure;
     contourf(squeeze(mean(cmg.lo_freqs,1)),...
         squeeze(mean(cmg.hi_freqs(:,:,1),1)),...
@@ -10,6 +11,18 @@ if nargin < 2
     colorbar();
 else
 
+    disp(col_levels)
+    if nargin < 5 || isempty(col_levels)
+        % Set colour levels based on the data
+        col_levels = 0:max(max(cmg.mi))/24:max(max(cmg.mi));
+    end
+
+    if nargin < 4 || isempty(log_high)
+        % Default to not using a log scale for the high frequency
+        log_high = false;
+    end
+
+    %% Extract the relevant parts of the spectrum
     lo_start = mean(cmg.lo_freqs(:,1));
     lo_stop = mean(cmg.lo_freqs(:,end));
     hi_start = mean(cmg.hi_freqs(:,1,1));
@@ -21,13 +34,15 @@ else
     hi_freq = freq_vect(hi_start < freq_vect & freq_vect < hi_stop);
     hi_pow = spectrum(hi_start < freq_vect & freq_vect < hi_stop);
 
+    %% Plot modulating frequency underneath the main comodulogram
     figure;
     axes('position',[.3,.1,.6,.2])
     plot(lo_freq,lo_pow)
     xlim([lo_start lo_stop]);
-    %ylim([0 max(lo_pow)*1.2]);
     xlabel('Modulating Frequency (Hz)');
 
+    %% Plot the modulated frequency spectrum vertically to the left of the
+    %  comodulogram
     axes('position',[.1,.3,.2,.6])
     plot(hi_freq,hi_pow)
     ylim([0 max(max(hi_pow))*1.2])
@@ -35,16 +50,17 @@ else
     set(gca, 'xdir', 'reverse');
     xlim([hi_start hi_stop]);
     xlabel('Modulated Frequency (Hz)');
+    % Use log scale if requested
+    if log_high == true; set(gca, 'YScale','log'); end
 
+    %% Plot the main comodulogram
     axes('position',[.3,.3,.6,.6])
     contourf(squeeze(mean(cmg.lo_freqs,1)),...
         squeeze(mean(cmg.hi_freqs(:,:,1),1)),...
-        cmg.mi');
+        cmg.mi',... 
+        'linestyle','none');
+    caxis([col_levels(1) col_levels(end)]);
     colormap('hot');
-    %colorbar();
     colorbar('position',[.92,.3,.03,.6]);
-    %ax = axes('position',[.92,.3,.1,.6]);
-    %set(gca,'visible','off');
-    %colorbar('peer',ax);
 
 end
