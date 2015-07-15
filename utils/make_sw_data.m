affa750893ecff4776f2478098512497d9e68eb7
Andrew Quinn <andrew.quinn@psych.ox.ac.uk>
2015-07-15 22:28:13 +0100

    Ensure nwindows in an int in utils/make_sw_data

--- utils/make_sw_data.m
+++ utils/make_sw_data.m
@@ -3,7 +3,7 @@ function [ data ] = make_sw_data(X,window_size,step)
 % Function for splitting a [ samples x epochs ] dataset in to [ samples x windows x epochs ] separate sliding windows based on a window length and step size (must be set in samples rather than ms)
 
 [nsamples,nepochs] = size(X);
-nwindows = floor ( (nsamples - window_size) / step);
+nwindows = fix ( (nsamples - window_size) / step);
 data = zeros(window_size,nwindows,nepochs);
 
 for idx = 1:nwindows
