987785f32dcfe53177bbe456ba71099a3984ef1e
Andrew Quinn <andrew.quinn@psych.ox.ac.uk>
2015-08-10 01:16:31 +0100

    Move loop over struct fields inside make_sw_data

--- utils/make_sw_data.m
+++ utils/make_sw_data.m
@@ -1,14 +1,36 @@
-function [ data ] = make_sw_data(X,window_size,step)
+function [ sw_signals ] = make_sw_data(signals,window_size,step)
 %
-% Function for splitting a [ samples x epochs ] dataset in to [ samples x windows x epochs ] separate sliding windows based on a window length and step size (must be set in samples rather than ms)
+% Function for splitting the data within a cfc signals struct into separate %
+% sliding windows based on a window length and step size (must be set in
+% samples rather than ms)
 
-[nsamples,nepochs] = size(X);
+[nchannels, nsamples] = size(signals.signal); % there should only be one channel here
 nwindows = fix ( (nsamples - window_size) / step);
-data = zeros(window_size,nwindows,nepochs);
 
-for idx = 1:nwindows
-    start_idx = (idx-1)*step + 1;
-    end_idx = (idx-1)*step + window_size; 
+fields = fieldnames(signals);
 
-    data(:,idx,:) = X(start_idx:end_idx,:);
+% We don't want to split these variables
+skip_field = {'hi_bounds','hi_bandwidth','hi_steps',...
+          'lo_bounds','lo_bandwidth','lo_steps','sr'};
+
+%% Main loop
+for i = 1:numel(fields)
+    if strmatch(fields{i},skip_field)
+        % Just add the variable without manipulating it
+        sw_signals.(fields{i}) = signals.(fields{i});
+    else
+        % Make sliding window version of variable
+        sw_data = zeros(nwindows,window_size);
+        data = signals.(fields{i});
+        for idx = 1:nwindows
+            start_idx = (idx-1)*step + 1;
+            end_idx = (idx-1)*step + window_size; 
+
+            sw_data(idx,:) = squeeze(data(:,start_idx:end_idx));
+        end
+
+        sw_signals.(fields{i}) = sw_data
+
+    end
 end
+
