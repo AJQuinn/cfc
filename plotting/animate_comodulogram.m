d9facedaa47f92f2ebd17e7d29ae71c25f85cfff
Andrew Quinn <andrew.quinn@psych.ox.ac.uk>
2015-08-23 21:52:35 +0100

    Update animate comodulogram

--- plotting/animate_comodulogram.m
+++ plotting/animate_comodulogram.m
@@ -1,9 +1,9 @@
-function animate_comodulogram( obj, signal, time_vect, outpath )
+function animate_comodulogram( obj, outpath )
 % Make a movie from a sliding window comodulogram
 
 
-ts_max = max(signal);
-ts_min = min(signal);
+ts_max = max(max(obj.signal));
+ts_min = min(min(obj.signal));
 if abs(ts_max) > abs(ts_min)
     ts_ylim = abs(ts_max) * 1.1;
 else
@@ -11,8 +11,8 @@ else
 end
 
 
-met_max = max(max(max(obj.mi))) * .45;
-%met_max = mean(mean(mean(obj.mi))) + ( std(std(std(obj.mi)))*2 );
+met_max = max(max(max(obj.mi_norm))) * .45;
+%met_max = mean(mean(mean(obj.mi_norm))) + ( std(std(std(obj.mi_norm)))*2 );
 
 
 figure;
@@ -20,20 +20,20 @@ figure;
 ax1 = axes('position',[.25,.1,.5,.25]);
 ax2 = axes('position',[.25,.43,.5,.55]);
 
-nwindows = size(obj.mi,1);
+nwindows = size(obj.mi_norm,1);
 
 for idx = 1:nwindows
-    
-    window_idx = time_vect >= obj.time_vect(1,idx) & ...
-        time_vect <= obj.time_vect(end,idx);
-    
-    plot(ax1,time_vect(window_idx),signal(window_idx));
-    xlim(ax1,[min(time_vect(window_idx)) max(time_vect(window_idx))]);
+
+    %window_idx = obj.time_vect(idx,:) >= obj.time_vect(idx,:) & ...
+    %    time_vect <= obj.time_vect(end,idx);
+
+    plot(ax1,obj.time_vect(idx,:),obj.signal(idx,:));
+    xlim(ax1,[min(obj.time_vect(idx,:)) max(obj.time_vect(idx,:))]);
     ylim(ax1,[-ts_ylim ts_ylim]);
     xlabel(ax1,'Time (seconds)');
-    
+
     contourf(mean(obj.lo_freqs),mean(mean(obj.hi_freqs,3)),...
-        squeeze(obj.mi(idx,:,:))','LineStyle','none',...
+        squeeze(obj.mi_norm(idx,:,:))','LineStyle','none',...
         'parent',ax2);
     caxis([0 met_max]);
     colormap('hot');
@@ -41,9 +41,9 @@ for idx = 1:nwindows
     xlabel(ax2,'Modulating Frequency (Hz)');
     ylabel(ax2,'Modulated Frequency (Hz)');
     ylabel(ax3,'Modulation Index');
-    
+
     print(sprintf('%s_%04d', outpath, idx),'-dpng');
-    
+
 end
 
 cmd = sprintf('convert -delay 15 -loop 0 %s %s', ...
