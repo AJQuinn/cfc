8929843dd3d4e121ec382fbd6be3e651dec92da7
Andrew Quinn <andrew.quinn@psych.ox.ac.uk>
2015-09-21 00:22:08 +0100

    Add a utility routine to get a single slice from within a cfc_signals struct

---
+++ utils/cfc_util_getslice.m
@@ -0,0 +1,30 @@
+function [ out ] = cfc_util_getslice( signals, start, stop )
+%% Extract one window from a cfc_signals struct
+%
+% signals: struct
+%   cfc signals structure
+% start: double
+%   start of the slice to extract
+% stop: double
+%   end of the slice to extract
+
+
+idx = signals.time_vect > start & signals.time_vect < stop;
+
+fields = fieldnames(signals);
+
+% We don't want to split these variables
+skip_field = {'hi_bounds','hi_bandwidth','hi_steps',...
+          'lo_bounds','lo_bandwidth','lo_steps','sr',...
+          'sample_rate','switching_freq'};
+
+out = [];
+for i = 1:numel(fields)
+    if strmatch(fields{i},skip_field)
+        % Just add the variable without manipulating it
+        out.(fields{i}) = signals.(fields{i});
+    else
+        tmp = signals.(fields{i});
+        out.(fields{i}) = tmp(:,start:stop,:);
+    end
+end
