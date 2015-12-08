e7dbe9b5ab5d54ad2f1db360258470a860b0473a
Andrew Quinn <andrew.quinn@psych.ox.ac.uk>
2015-12-08 12:46:41 +0000

    Update input arguments into cfc_plot_sw/cmg

--- plotting/cfc_plot_sw.m
+++ plotting/cfc_plot_sw.m
@@ -1,4 +1,4 @@
-function cfc_plot_sw( cfc_results )
+function cfc_plot_sw( cfc_results, metric )
 
 time = cfc_results.time_vect;
 thresh = nan;
@@ -7,32 +7,32 @@ for met_idx = 1:length(cfc_results.cfg.metrics)
     if strcmp(cfc_results.cfg.metrics{met_idx},'ESC')
         metric_name = 'ESC';
         metric = cfc_results.esc;
-        metric_nulls = cfc_results.esc_null;
+        if isfield(cfc_results,'esc_null'); metric_nulls = cfc_results.esc_null;end
         if isfield(cfc_results,'esc_thresh');thresh = cfc_results.esc_thresh;end
     elseif strcmp(cfc_results.cfg.metrics{met_idx},'NESC')
         metric_name = 'NESC';
         metric = cfc_results.nesc;
-        metric_nulls = cfc_results.nesc_null;
+        if isfield(cfc_results,'nesc_null'); metric_nulls = cfc_results.nesc_null;end
         if isfield(cfc_results,'nesc_thresh');thresh = cfc_results.nesc_thresh;end
     elseif strcmp(cfc_results.cfg.metrics{met_idx},'AEC')
         metric_name = 'AEC';
         metric = cfc_results.aec;
-        metric_nulls = cfc_results.aec_null;
+        if isfield(cfc_results,'aec_null'); metric_nulls = cfc_results.aec_null;end
         if isfield(cfc_results,'aec_thresh');thresh = cfc_results.aec_thresh;end
     elseif strcmp(cfc_results.cfg.metrics{met_idx},'PLV')
         metric_name = 'PLV';
         metric = cfc_results.plv;
-        metric_nulls = cfc_results.plv_null;
+        if isfield(cfc_results,'plv_null'); metric_nulls = cfc_results.plv_null;end
         if isfield(cfc_results,'plv_thresh');thresh = cfc_results.plv_thresh;end
     elseif strcmp(cfc_results.cfg.metrics{met_idx},'GLM')
         metric_name = 'GLM';
         metric = cfc_results.glm;
-        metric_nulls = cfc_results.glm_null;
+        if isfield(cfc_results,'glm_null'); metric_nulls = cfc_results.glm_null;end
         if isfield(cfc_results,'glm_thresh');thresh = cfc_results.glm_thresh;end
     elseif strcmp(cfc_results.cfg.metrics{met_idx},'MI')
         metric_name = 'MI';
         metric = cfc_results.mi;
-        metric_nulls = cfc_results.mi_null;
+        if isfield(cfc_results,'mi_null'); metric_nulls = cfc_results.mi_null;end
         if isfield(cfc_results,'mi_thresh');thresh = cfc_results.mi_thresh;end
     else
         fprintf('CFC Metric %s not recognised!\nPlease choose from:\nESC, NESC, AEC, PLV, GLM and MI',cfg.metrics{met_idx});
@@ -40,6 +40,12 @@ for met_idx = 1:length(cfc_results.cfg.metrics)
 
     % Sliding windows
     figure;
+    if exist('metric_nulls') == 1
+        subplot(211)
+    else
+        subplot(111)
+    end
+
     plot(time,metric)
     hold on
     title(sprintf('Cross frequency coupling between %dHz and %dHz',...
@@ -56,43 +62,44 @@ for met_idx = 1:length(cfc_results.cfg.metrics)
         legend({metric_name});
     end
 
-    % Histograms
-    bins = 0:max(metric)/24:max(metric);
-    figure
-    subplot(211)
-    [n1, xout1] = hist(metric_nulls,bins);
-    bar(xout1,n1,'r'); grid; hold on;
-    ylabel('Frequency');
+    if exist('metric_nulls') == 1
+        % Histograms
+        bins = 0:max(metric)/24:max(metric);
+        subplot(223)
+        [n1, xout1] = hist(metric_nulls,bins);
+        bar(xout1,n1,'r'); grid; hold on;
+        ylabel('Frequency');
 
-    if ~isnan(thresh)
-        ybounds = ylim;
-        plot([thresh(3) thresh(3)],[ybounds(1) ybounds(2)]);
-        plot([thresh(2) thresh(2)],[ybounds(1) ybounds(2)]);
-        plot([thresh(1) thresh(1)],[ybounds(1) ybounds(2)]);
-        legend({'Null Distribution','Thresh .001','Thresh .01','Thresh .05'});
-    else
-        legend({'Null Distribution'});
+        if ~isnan(thresh)
+            ybounds = ylim;
+            plot([thresh(3) thresh(3)],[ybounds(1) ybounds(2)]);
+            plot([thresh(2) thresh(2)],[ybounds(1) ybounds(2)]);
+            plot([thresh(1) thresh(1)],[ybounds(1) ybounds(2)]);
+            legend({'Null Distribution','Thresh .001','Thresh .01','Thresh .05'});
+        else
+            legend({'Null Distribution'});
+        end
+
+        subplot(224)
+        [n2, xout2] = hist(metric,bins);
+        bar(xout2,n2,'g'); grid; hold on;
+
+        if ~isnan(thresh)
+            ybounds = ylim;
+            plot([thresh(3) thresh(3)],[ybounds(1) ybounds(2)]);
+            plot([thresh(2) thresh(2)],[ybounds(1) ybounds(2)]);
+            plot([thresh(1) thresh(1)],[ybounds(1) ybounds(2)]);
+            legend({'Observed Distribution','Thresh .001','Thresh .01','Thresh .05'});
+        else
+            legend({'Observed Distribution'});
+        end
+
+        xlabel(metric_name);
+        ylabel('Frequency');
+        title('Null and observed distributions');
+
+        thresh = nan;
     end
-
-    subplot(212)
-    [n2, xout2] = hist(metric,bins);
-    bar(xout2,n2,'g'); grid; hold on;
-
-    if ~isnan(thresh)
-        ybounds = ylim;
-        plot([thresh(3) thresh(3)],[ybounds(1) ybounds(2)]);
-        plot([thresh(2) thresh(2)],[ybounds(1) ybounds(2)]);
-        plot([thresh(1) thresh(1)],[ybounds(1) ybounds(2)]);
-        legend({'Observed Distribution','Thresh .001','Thresh .01','Thresh .05'});
-    else
-        legend({'Observed Distribution'});
-    end
-
-    xlabel(metric_name);
-    ylabel('Frequency');
-    title('Null and observed distributions');
-
-    thresh = nan;
 
 end
 
