dc49349def1906fa482455ba4b7bb857512d2032
Andrew Quinn <andrew.quinn@psych.ox.ac.uk>
2016-08-23 18:24:14 +0100

    Update default filter order values

--- +cfc/+filter/checkcfg.m
+++ +cfc/+filter/checkcfg.m
@@ -12,11 +12,12 @@ end
 
 %% Set filter order
 % default value is 3 or 7 times the centre frequency for low/high freqs
-if ~isfield(filt_cfg,'order')
-    if filt_cfg.centre_freq > 20
-        filt_cfg.order = fix(1/filt_cfg.centre_freq * filt_cfg.sample_rate) * 7;
+if ~isfield(filt_cfg,'order') || isempty(filt_cfg.order)
+    lo_freq = filt_cfg.centre_freq - .5*filt_cfg.pass_width;
+    if lo_freq > 10
+        filt_cfg.order = fix(1/lo_freq * filt_cfg.sample_rate) * 10;
     else
-        filt_cfg.order = fix(1/filt_cfg.centre_freq * filt_cfg.sample_rate) * 3;
+        filt_cfg.order = fix(1/lo_freq * filt_cfg.sample_rate) * 3;
     end
 end
 
